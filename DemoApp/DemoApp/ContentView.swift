3045ee4f427b7e0c139dfbab06eae0835f444a71
Developer <ritishka.sharma@iphtechnologies.com>
2022-05-05 18:52:20 +0530

    Revert "initial commit"
    This reverts commit e6d9c1a8ce8230e22cd8c13c951c52ca28467678.

---
+++ DemoApp/DemoApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  DemoApp
+//
+//  Created by IPHTECH 4 on 21/04/22.
+//
+
+import SwiftUI
+import Speech
+import SwiftSpeech
+import Combine
+
+struct ContentView: View {
+    var body: some View {
+        SearchView()   // loading search view
+            
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
