805e2ce22d151474940f5f30fbd45bb425dfc923
DeveloperIphtech <96408807+DeveloperIphtech@users.noreply.github.com>
2022-05-06 12:41:34 +0530

    initial commit

---
+++ TalkToMe/TalkToMeView/StopRecord.swift
@@ -0,0 +1,24 @@
+//
+//  StopRecord.swift
+//  DemoApp
+//
+//  Created by IPHTECH 4 on 27/04/22.
+//
+
+import Foundation
+import SwiftSpeech
+import Speech
+import Combine
+import ComposableArchitecture
+import AVFoundation
+
+let synthesizer = AVSpeechSynthesizer()
+// function to convert text to speech
+func startSpeaking() {
+    let utterance = AVSpeechUtterance(string: result)  //storing text to convert to speech
+    utterance.voice = AVSpeechSynthesisVoice(language: "en-US")  //language for speech
+    utterance.rate = 0.45  //managing speed rate of speech
+    synthesizer.continueSpeaking()  //continuing new speech
+    synthesizer.speak(utterance)  //converting text to speech
+}
+
